ff5d0b101af4f2e2007c202fd57867dd3ce6e8a9
MaxiCode <andresholsbach@gmail.com>
2022-05-26 21:49:37 -0300

    first commit

---
+++ taller/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Equipo(models.Model):
+    modelo = models.CharField(max_length=40)
+    serie = models.CharField(max_length=40)
+    contador = models.IntegerField()
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    telefono = models.IntegerField()
+    email = models.EmailField()
+
+class Tecnico(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    telefono = models.IntegerField()
+    email = models.EmailField()
+
+# Create your models here.
